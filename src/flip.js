5fab15d19c46d70e9ac26ad811a5da83690b5da8
FreeMasen <RFM@Herman.local>
2016-12-22 06:30:19 -0600

    Wed Morning

--- src/flip.js
+++ src/flip.js
@@ -1,11 +1,15 @@
 const {exec} = require('child_process')
-
+const os = require('os')
 function flip(code) {
-    exec('./codesend', [code], (err, stdout, stderr) => {
-        console.log(err)
-        console.log(stdout)
-        console.log(stderr)
-    })
+    if (os.platform() == 'linux') {
+        exec('./codesend', [code], (err, stdout, stderr) => {
+            console.log(err)
+            console.log(stdout)
+            console.log(stderr)
+        })
+    } else {
+        console.log(`./ codesend ${code}`)
+    }
 }
 
 module.exports = flip
