e2c7b5ae475a64619dd3a43f7486ec16c2f5acec
rfm <r.f.masen@gmail.com>
2016-12-23 12:55:59 -0600

    remove hammerjs path

--- systemjs.config.js
+++ systemjs.config.js
@@ -26,7 +26,7 @@
       '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'hammerjs':'npm:@types/hammerjs/index.d.js'
+      //'hammerjs':'npm:@types/hammerjs/index.d.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
