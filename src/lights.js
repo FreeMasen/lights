545cb6d239a8f28106bdf1cfbd750e2e71422b1d
FreeMasen <RFM@Herman.local>
2016-12-28 12:12:51 -0600

    rework to lightmanager instead of just data

--- src/lights.js
+++ src/lights.js
@@ -1,108 +1,146 @@
-const defaultDays = {
-    m: false,
-    t: false,
-    w: false,
-    r: false,
-    f: false,
-    s: false,
-    u: false
-}
-const morningOn = {
-    isOn: true,
-    time: {
-        hour: 5,
-        minute: 30,
-        am: true
-    },
-    days: defaultDays
+const fs = require('fs')
+
+function LightManager() {
+    this._readInLights()
 }
 
-const morningOff = {
-    isOn: false,
-    time: {
-        hour: 7,
-        minute: 3,
-        am: true
+LightManager.prototype._readInLights = function(retry) {
+    try {
+        this.lights = fs.readFileSync('data/lights.json')
+    } catch (e) {
+        if (retry) throw e
+        this._createDir()
+        this._createDefaultFile()
+        this._readInLights(1)
+    }
+}
+
+LightManager.prototype._createDir = function() {
+    try {
+        fs.mkdirSync('data')
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+LightManager.prototype._createDefaultFile = function() {
+    const defaultDays = {
+        m: false,
+        t: false,
+        w: false,
+        r: false,
+        f: false,
+        s: false,
+        u: false
+    }
+    const morningOn = {
+        isOn: true,
+        time: {
+            hour: 5,
+            minute: 30,
+            am: true
         },
-    days: defaultDays
-}
+        days: defaultDays
+    }
 
-const eveningOn = {
-    isOn: true,
-    time: {
-    hour: 5,
-    minute: 0,
-    am: false
-    },
-    days: defaultDays
-}
+    const morningOff = {
+        isOn: false,
+        time: {
+            hour: 7,
+            minute: 3,
+            am: true
+            },
+        days: defaultDays
+    }
 
-const eveningOff = {
-    isOn: false,
-    time: {
-        hour: 11,
+    const eveningOn = {
+        isOn: true,
+        time: {
+        hour: 5,
         minute: 0,
         am: false
-    },
-    days: defaultDays
-}
+        },
+        days: defaultDays
+    }
 
-const switchOne = {
-    name: 'Christmas Tree',
-    id: 1,
-    on: true,
-    codes: {
-        '1': '4543795',
-        '0': '4543804'
-    }, 
-    timers: [
-        morningOn,
-        morningOff,
-        eveningOn,
-        eveningOff
-]}
+    const eveningOff = {
+        isOn: false,
+        time: {
+            hour: 11,
+            minute: 0,
+            am: false
+        },
+        days: defaultDays
+    }
 
-const switchTwo = {
-    name: 'Not In Use',
-    id: 2,
-    codes: {
-        '1': '4543939',
-        '0': '44543948'
-    },
-    timers: []
-}
-
-const switchThree = {
-        name: 'Not In Use',
-    id: 2,
-    codes: {
-        '1': '4544359',
-        '0': '4544268'
-    },
-    timers: []
-}
-
-const switchFour = {
-    name: 'Breakfast Nook',
-    id: 4,
-    on: false,
-    codes: {
-        '1': '4545795',
-        '0': '4545804'
-    },
-    timers: []
-}
-
-const switchFive = {
-    name: 'Craft Room',
-    id: 5,
-    on: false,
-    codes: {
-        '1': '4551939',
-        '0': '4551948'
-    },
-    timers: [
-
+    const switchOne = {
+        name: 'Switch One',
+        id: 1,
+        on: true,
+        codes: {
+            '1': '4543795',
+            '0': '4543804'
+        }, 
+        timers: [
+            morningOn,
+            morningOff,
+            eveningOn,
+            eveningOff
     ]}
 
-module.exports = [switchOne, switchTwo, switchThree, switchFour, switchFive]
+    const switchTwo = {
+        name: 'Switch Two',
+        id: 2,
+        codes: {
+            '1': '4543939',
+            '0': '44543948'
+        },
+        timers: []
+    }
+
+    const switchThree = {
+            name: 'Switch Three',
+        id: 2,
+        codes: {
+            '1': '4544359',
+            '0': '4544268'
+        },
+        timers: []
+    }
+
+    const switchFour = {
+        name: 'Switch Four',
+        id: 4,
+        on: false,
+        codes: {
+            '1': '4545795',
+            '0': '4545804'
+        },
+        timers: []
+    }
+
+    const switchFive = {
+        name: 'Switch Five',
+        id: 5,
+        on: false,
+        codes: {
+            '1': '4551939',
+            '0': '4551948'
+        },
+        timers: [
+
+        ]}
+
+    this.saveLights([switchOne, switchTwo, switchThree, switchFour, switchFive])
+}
+
+LightManager.prototype.saveLights = function(newLights) {
+    try {
+        fs.writeFileSync('data/lights.json', JSON.stringify(newLights))
+        this._readInLights()
+    } catch (e) {
+        conosle.log(e)
+    }
+}
+
+module.exports = LightManager
