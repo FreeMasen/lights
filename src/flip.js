b7ef19ce96f363553431d0d6255c5dbe2f65d2a0
rfm <r.f.masen@gmail.com>
2016-12-30 15:23:42 -0600

    remove all ./codesend error throwing.

--- src/flip.js
+++ src/flip.js
@@ -3,11 +3,12 @@ const os = require('os')
 function flip(code) {
     if (os.platform() == 'linux' ||
         os.platform() == 'openbsd') {
+        //TODO: troubleshoot calls to codesend
         exec('./codesend', [code], (err, stdout, stderr) => {
             if (err) {
-                throw err
+                //throw err
             }
-            //TODO: troubleshoot calls to codesend
+            
             if (stderr) {
                 //throw new Error(stderr)
             }
