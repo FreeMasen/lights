01a78ab19bacf4ddd7a0a5467fcaa7ffb44c3761
rfm <r.f.masen@gmail.com>
2017-01-03 16:06:36 -0600

    rework UI.detail.test.js

--- test/UI.detail.test.js
+++ test/UI.detail.test.js
@@ -2,11 +2,10 @@ const Nightmare = require('nightmare')
 const assert = require('assert')
 const url = 'http://localhost:9999'
 
-
 describe('UI.Switch Detail', function() {
     this.timeout(30000)
     it('should render after clicking on the fab', function(done) {
-        new Nightmare()
+        new Nightmare({executionTimeout: 3000})
             .goto(url)
             .wait('.switch')
             .click('.set-button')
@@ -14,29 +13,36 @@ describe('UI.Switch Detail', function() {
             .evaluate(function() {
                 return document.getElementById('title').value
             })
+            .end()
             .then(function(result) {
                 assert(result != undefined, 'Title innerHTML was undfined')
                 assert(result == 'Switch One', `Title was not Switch One: ${result}`)
+                
                 done()
             }).catch(function(err) {
+                
                 done(err)
             })
     })
     it('+ should increase the number of timers', function(done) {
         new Nightmare()
-            .wait('.switch')
+            .goto(url)
+            .wait('.set-button')
             .click('.set-button')
             .wait('#add-button')
             .click('#add-button')
             .evaluate(function() {
                 return document.getElementsByClassName('timer').length
             })
+            .end()
             .then(function(result) {
                 assert(result != undefined, 'result was undefined')
                 assert(result == 5, `timers.length was not equal 5: ${result}`)
+                
                 done()
             })
             .catch(function(err) {
+                
                 done(err)
             })
     })
