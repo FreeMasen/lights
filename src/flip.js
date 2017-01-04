ed6bb46c32365cf06474ddff591095e8d5dd9599
rfm <r.f.masen@gmail.com>
2017-01-04 14:58:43 -0600

    remove open bsd from flip

--- src/flip.js
+++ src/flip.js
@@ -1,8 +1,7 @@
 const {exec} = require('child_process')
 const os = require('os')
 function flip(code) {
-    if (os.platform() == 'linux' ||
-        os.platform() == 'openbsd') {
+    if (os.platform() == 'linux') {
         //TODO: troubleshoot calls to codesend
         exec(`./codesend ${code}`, [], (err, stdout, stderr) => {
             
